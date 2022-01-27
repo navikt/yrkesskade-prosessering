ed27975edde23a430f7686486144da03d13dc808
Jone Lura <jone.lura@nav.no>
2022-01-27 12:15:09 +0100

    JWT token validering

--- src/backend/tokenUtils.ts
+++ src/backend/tokenUtils.ts
@@ -3,23 +3,19 @@ import jwt from 'jsonwebtoken';
 import jwkToPem from 'jwk-to-pem';
 
 const loggOgReturnerOmTokenErGyldig = (req: Request, key: string, validAccessToken: boolean) => {
-    console.log(
-        req,
-        `Har ${validAccessToken ? 'gyldig' : 'ikke gyldig'} token for key '${key}'`
-    );
+    console.log(req, `Har ${validAccessToken ? 'gyldig' : 'ikke gyldig'} token for key '${key}'`);
     return validAccessToken;
 };
 
 export const hasValidAccessToken = (req: Request) => {
-
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-        return false
+        return false;
     }
 
     const token = authHeader.split(' ')[1];
-    
+
     if (!token) {
         return false;
     }
@@ -27,18 +23,27 @@ export const hasValidAccessToken = (req: Request) => {
     const jwks = process.env.AZURE_APP_JWKS;
     const clientId = process.env.AZURE_APP_CLIENT_ID;
     const jwk = process.env.AZURE_APP_JWK;
-    console.log('Token: ', token);
-    
+    console.log(token);
+
     console.log('JWK: ', jwk);
-    
+
     const pem = jwkToPem(JSON.parse(jwk), { private: true });
-    
+
     console.log('PEM', pem);
-    
-    jwt.verify(token, pem, function(err, decoded) {
-        console.log('error: ', err);        
-        console.log('decoded:' ,decoded)
-    });
-    
+
+    jwt.verify(
+        token,
+        pem,
+        {
+            algorithms: ['RS256'],
+            audience: process.env.AZURE_APP_CLIENT_ID,
+            issuer: process.env.AZURE_OPENID_CONFIG_ISSUER,
+        },
+        (err, decoded) => {
+            console.log('error: ', err);
+            console.log('decoded:', decoded);
+        }
+    );
+
     return true;
 };
