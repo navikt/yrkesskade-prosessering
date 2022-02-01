aea8d0208294fd7fbb251408d191183a1c2bc459
Jone Lura <jone.lura@nav.no>
2022-02-01 10:26:00 +0100

    yrkesskade-prosessering
    - lag on behalf of token

--- src/backend/session.ts
+++ src/backend/session.ts
@@ -1,14 +1,9 @@
 import { Express } from 'express';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import redis from 'redis';
-//import { appConfig } from '../config';
 import { ISessionKonfigurasjon } from './typer';
 import { PassportStatic } from 'passport';
 
-/* tslint:disable */
-const RedisStore = require('connect-redis')(session);
-/* tslint:enable */
 
 export default (
     app: Express,
@@ -18,53 +13,18 @@ export default (
     app.use(cookieParser(sessionKonfigurasjon.cookieSecret));
     app.set('trust proxy', 1);
 
-    if (sessionKonfigurasjon.redisUrl) {
-        console.log('Setter opp redis for session');
+    console.log('Setter opp in-memory db for session');
 
-        const client = redis.createClient({
-            db: 1,
-            host: sessionKonfigurasjon.redisUrl,
-            password: sessionKonfigurasjon.redisPassord,
-            port: 6379,
-        });
-        client.unref();
+    app.use(
+        session({
+            cookie: { sameSite: 'lax', secure: sessionKonfigurasjon.secureCookie },
+            name: sessionKonfigurasjon.navn,
+            resave: false,
+            saveUninitialized: false,
+            secret: sessionKonfigurasjon.cookieSecret,
+        }),
+    );
 
-        const store = new RedisStore({
-            client,
-            disableTouch: true,
-            ttl: sessionKonfigurasjon.sessionMaxAgeSekunder,
-        });
-
-        app.use(
-            session({
-                cookie: {
-                    maxAge: sessionKonfigurasjon.sessionMaxAgeSekunder
-                        ? sessionKonfigurasjon.sessionMaxAgeSekunder * 1000
-                        : undefined,
-                    sameSite: 'lax',
-                    secure: sessionKonfigurasjon.secureCookie,
-                },
-                unset: 'destroy',
-                name: sessionKonfigurasjon.navn,
-                resave: false,
-                saveUninitialized: false,
-                secret: sessionKonfigurasjon.cookieSecret,
-                store,
-            }),
-        );
-    } else {
-        console.log('Setter opp in-memory db for session');
-
-        app.use(
-            session({
-                cookie: { sameSite: 'lax', secure: sessionKonfigurasjon.secureCookie },
-                name: sessionKonfigurasjon.navn,
-                resave: false,
-                saveUninitialized: false,
-                secret: sessionKonfigurasjon.cookieSecret,
-            }),
-        );
-    }
 
     app.use(passport.initialize());
     app.use(passport.session());
